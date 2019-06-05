c5da27619b46107f39ccf059d2bb15f86f878fe7
Sanchez <fsanchez@1usmfsanchez1.local>
2019-06-05 16:13:07 -0400

    Added author models

---
+++ app/models/author.rb
@@ -0,0 +1,3 @@
+class Author < Sequel::Model
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -6,6 +6,13 @@ Sequel.migration do
       column :age, "integer"
     end
     
+    create_table(:book_tables) do
+      primary_key :id
+      column :name, "varchar(255)"
+      column :genre, "varchar(255)"
+      column :author_id, "integer"
+    end
+    
     create_table(:schema_info) do
       column :version, "integer", :default=>0, :null=>false
     end
@@ -13,6 +20,6 @@ Sequel.migration do
 end
 Sequel.migration do
   change do
-    self << "INSERT INTO `schema_info` (`version`) VALUES (1)"
+    self << "INSERT INTO `schema_info` (`version`) VALUES (2)"
   end
 end
