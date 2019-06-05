ae1ee61a5c0645f9dba387658cd3b47ff951998d
Sanchez <fsanchez@1usmfsanchez1.local>
2019-06-05 17:59:39 -0400

    Added author type to book_type

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -2,5 +2,6 @@ module Types
   class BookType < Types::BaseObject
     field :name, String, null: true
     field :genre, String, null: true
+    field :author, Types::AuthorType, null: false
   end
 end
